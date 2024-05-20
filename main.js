c45ff6ec6535fb4fe4f433f514194a50272dbb40
mtrideveloper <unitydev2d@gmail.com>
2024-05-20 11:32:22 +0700

    Base

---
+++ main.js
@@ -0,0 +1,30 @@
+// simple p2p chat with PeerJS
+const peer = new Peer();
+let conn = null
+
+peer.on('open', function (id) {
+    console.log('My peer ID is: ' + id)
+})
+
+document.getElementById('connectButton').addEventListener('click', function () {
+    const peerId = document.getElementById('peerIdInput').value
+    conn = peer.connect(peerId)
+    conn.on('open', function () {
+        conn.on('data', function (data) {
+            document.getElementById('messages').value += 'Họ: ' + data + '\n'
+        })
+    })
+})
+
+document.getElementById('sendButton').addEventListener('click', function () {
+    const message = document.getElementById('messageInput').value
+    document.getElementById('messages').value += 'Bạn: ' + message + '\n'
+    conn.send(message)
+})
+
+peer.on('connection', function (connection) {
+    conn = connection
+    conn.on('data', function (data) {
+        document.getElementById('messages').value += 'Họ: ' + data + '\n'
+    })
+})
